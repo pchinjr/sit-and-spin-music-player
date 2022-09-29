b34b63d206399dd7d419950ea47f9233513d971b
Paul Chin Jr <pchinjr@gmail.com>
2022-09-28 20:00:55 -0400

    added audio player, default song, and playback rate controll

--- server.js
+++ server.js
@@ -2,6 +2,41 @@
 const fastify = require('fastify')({ logger: true })
 const path = require('path')
 
+// it about to get ugly, starting to add J5 logic here
+// const { Board, Sensor, Led } = require('johnny-five')
+
+// const board = new Board()
+
+// board.on('ready', () => {
+//   const digitalSensor = new Sensor({
+//     "pin": 9,
+//     "type": 'digital'
+//   })
+
+//   const time = Date.now()
+//   const led = new Led(13);
+//   const revolutions = 0
+
+//   digitalSensor.on('change', value => {
+//     if (value === 0) {
+//       led.on()
+//       revolutions += 1
+//     } else {
+//       led.off()
+//     }
+//   })
+
+//   // loop every second, during that second, collect all revolutions and multiply by 60
+//   setInterval(() => {
+//     let elapsed = Date.now() - time
+//     console.log("Time Elapsed(ms) : ", elapsed)
+//     console.log("RPM : ", revolutions * 60) // problem: push this into a socket
+//     console.log("-----------------")
+//     revolutions = 0
+//   }, 1000);
+
+// })
+
 fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'public'),
   prefix: '/public/'
@@ -16,6 +51,15 @@ fastify.get('/', async (request, reply) => {
 fastify.register(async function (fastify) {
   fastify.get('/ws', { websocket: true }, (connection, request) => {
     console.log('Client connected')
+    // note: count resets on each connection
+    // let count = 0
+
+    setInterval(() => {
+      const randomInterval = () => {
+        return (Math.floor(Math.random() * (200 - 25 + 1)) + 25)/100
+      }
+      connection.socket.send(`${randomInterval()}`)
+    }, 3000)
 
     connection.socket.on('message', message => {
       console.log(`Client message: ${message}`)
